38d967c5c00f043379b5dc6ec8bc7ef87e7ffeea
joeyfboone <joey@urgentmail.com>
2017-08-29 09:59:29 -0400

    initial var setup

---
+++ assets/javascript/script.js
@@ -0,0 +1,32 @@
+var MyQuestion = [{
+    question: "Question 1",
+    answers: {
+        a:"A Answer",
+        b:"B Answer",
+        c:"C Answer",
+        d:"D Answer"
+    },
+    correctAnswer: "a"
+},
+{
+    question: "Question 2",
+    answers: {}
+        a:"A Answer",
+        b:"B Answer",
+        c:"C Answer",
+        d:"D Answer"
+    },
+    correctAnswer: "b"
+},
+{
+    question: "Question 3",
+    answers: {}
+        a:"A Answer",
+        b:"B Answer",
+        c:"C Answer",
+        d:"D Answer"
+    },
+    correctAnswer: "c"
+},
+]
+
