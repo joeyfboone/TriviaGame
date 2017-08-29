dec5784aa23fcf6da11e9a2cb5e4069e9141068e
joeyfboone <joey@urgentmail.com>
2017-08-29 19:52:14 -0400

    Timer Completed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,4 @@
+var number = 30;
 var MyQuestion = [{
     question: "Question 1",
     answers: {
@@ -10,7 +11,7 @@ var MyQuestion = [{
 },
 {
     question: "Question 2",
-    answers: {}
+    answers: {
         a:"A Answer",
         b:"B Answer",
         c:"C Answer",
@@ -20,7 +21,7 @@ var MyQuestion = [{
 },
 {
     question: "Question 3",
-    answers: {}
+    answers: {
         a:"A Answer",
         b:"B Answer",
         c:"C Answer",
@@ -30,3 +31,62 @@ var MyQuestion = [{
 },
 ]
 
+$("#start").on("click", function() {
+    console.log("start")
+     play();
+})
+
+        function play (){
+        run();
+        var qtext = "";
+        for (var i = 0; i < MyQuestion.length; i++) {
+            console.log(MyQuestion[i]);
+            qtext = MyQuestion.question;
+            $("#question").html("qtext " + i + ": "+qtext);
+
+        }
+        }
+
+
+
+
+
+
+
+
+
+        function run() {
+            intervalId = setInterval(decrement, 1000);
+        }
+      
+          //  The decrement function.
+          function decrement() {
+      
+            //  Decrease number by one.
+            number--;
+      
+            //  Show the number in the #show-number tag.
+            $("#timer").html("<h2>"+"Timer: " + number + "</h2>");
+      
+      
+            //  Once number hits zero...
+            if (number === 0) {console.log ("end")}
+
+            
+              console.log(number);
+            }
+
+
+
+   //           $("#start").on("click", function() {
+  //              $('#status').text('Status: In Progress')
+  //              total_value = total_value + diamond_1_value;
+  //                  console.log ("total value:" + total_value);
+  //                  $("#total_value").html("Total Value: "+total_value);
+  //                  $("#target_no").html("Target Number: "+random_target);
+  ///                  test();
+  //               })
+            
+            
+           //          function play (){
+  //              if (total_value < random_target) {}}}   
